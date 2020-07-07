e16320ab8d5e2f642d2f97dbb461fb29accf2114
kevinn-12 <vdigueslacerda@gmail.com>
2020-07-07 15:49:00 -0300

    Sampling lines in txt files. Corpus created. Initiated exploratory analysis. Commit before n-gram attempt.

--- Capstone.R
+++ Capstone.R
@@ -1,5 +1,5 @@
-library(stringr); library(tm); library(dplyr); library(tibble); library(reshape); library(caret); library(RWeka)
-
+library(stringr); library(tm); library(dplyr); library(tibble); library(reshape); library(caret); library(RWeka); library(LaF)
+library(jsonlite); library(purrr); library(data.table); library(tidyr); library(ggplot2)
 
 # Importing and CLeaning Data
 if (! file.exists("Coursera-SwiftKey.zip")) {
@@ -7,47 +7,60 @@ if (! file.exists("Coursera-SwiftKey.zip")) {
         unzip("Coursera-SwiftKey.zip")
 }
 
-folders <- list.files(path = "final", full.names = TRUE, recursive = FALSE)
-names <- str_extract(folders, "[^/]*$")
-lang <- str_extract(folders, "[^_]*$")
+files <- list.files(pattern = "\\.txt$", recursive = T)
 
-for (i in 1:length(folders)) {
-        assign(names[i], VCorpus(DirSource(folders[i], encoding = "UTF-8"), readerControl = list(language = lang[i])))
+set.seed(123)
+for (i in 1:length(files)) {
+        assign(files[i], sample_lines(files[i], 1000))
 }
 
-for (i in 1:length(folders)) {
-        assign(names[i], tm_map(get(names[i]), content_transformer(tolower)))
-        assign(names[i], tm_map(get(names[i]), content_transformer(removePunctuation)))
-        assign(names[i], tm_map(get(names[i]), content_transformer(removeNumbers)))
-        assign(names[i], tm_map(get(names[i]), content_transformer(stripWhitespace)))
-}
+dat <- c(mget(unlist(files)))
 
+rm(list = files)
 
-for (i in 1:length(names)) {
-        assign(names[i],
-               data.frame(
-                       Language = lang[i],
-                       Text = unlist(sapply(get(names[i]), `[`, "content")),
-                       stringsAsFactors = FALSE))
-}
-
-for (i in 1:length(names)) {
-        assign(names[i], get(names[i]) %>%
-                       rownames_to_column(., "File"))
-}
-
-dat <- reshape::merge_all(list(de_DE, en_US, ru_RU, fi_FI))
-
-rm(de_DE, en_US, ru_RU, fi_FI, folders, i, lang, names)
+dat <- map(dat, as.data.frame)
+dat <- rbindlist(dat, fill = T, idcol = T)
 
 dat <- dat %>%
-        mutate(., File = as.factor(gsub("^.+?\\.(.+?)\\..*$", "\\1", File)),
-               Language = as.factor(Language))
+        dplyr::rename(., doc_id = .id, text = `.x[[i]]`) %>%
+        mutate(., doc_id = as.factor(sub(".*\\_ *(.*?) *.txt*", "\\1", doc_id))) 
 
-# Tokenization
+dat <- dat %>%
+        group_by(., doc_id) %>%
+        summarise(., text = Reduce(paste, text)) %>%
+        mutate(., language = sub("\\..*", "", doc_id)) %>%
+        as.data.frame(.) %>%
+        DataframeSource(.) %>%
+        VCorpus(.)
 
+dat <- dat %>%
+        tm_map(., content_transformer(tolower)) %>%
+        tm_map(., content_transformer(removePunctuation)) %>%
+        tm_map(., content_transformer(removeNumbers)) %>%
+        tm_map(., content_transformer(stripWhitespace)) %>%
+        tm_map(., content_transformer(PlainTextDocument)) %>%
+        DocumentTermMatrix(.) %>%
+        removeSparseTerms(., 0.99)
+        
+# Exploratory Analysis 
+top_words <- dat %>%
+        as.matrix(.) %>%
+        as.data.frame(.) %>%
+        rownames_to_column(., var = "Doc")
 
+top_words <- top_words %>%
+        gather(., Word, Frequency, 2:ncol(top_words)) %>%
+        mutate(., Language = str_extract(Doc, "[^.]+")) %>%
+        mutate_if(., is.character, as.factor) %>%
+        group_by(., Language, Word) %>%
+        summarise(., Frequency = sum(Frequency)) %>%
+        data.frame(.) %>%
+        group_by(., Language) %>%
+        top_n(., 10, Frequency) %>%
+        data_frame(.)
+        #arrange(., desc(Frequency), group_by = Language)
 
-
-
+freq.plot <- ggplot(top_words, aes(x = reorder(Word, -Frequency), y = Frequency, fill = Language)) + geom_bar(stat="identity", position="dodge") +
+        facet_wrap(~ Language, ncol = 2, scales='free_x') + theme_bw() + labs(title = "Top 10 Words by Language", x = "Words")
+freq.plot 
 
