139d20532b0bddece5452468000ed318a6eace04
kevinn-12 <vdigueslacerda@gmail.com>
2020-07-07 20:03:06 -0300

    Added individual corpus by language.

--- Capstone.R
+++ Capstone.R
@@ -14,42 +14,61 @@ for (i in 1:length(files)) {
         assign(files[i], sample_lines(files[i], 1000))
 }
 
-dat <- c(mget(unlist(files)))
+dat_all <- c(mget(unlist(files)))
 
 rm(list = files)
 
-dat <- map(dat, as.data.frame)
-dat <- rbindlist(dat, fill = T, idcol = T)
+dat_all <- map(dat_all, as.data.frame)
+dat_all <- rbindlist(dat_all, fill = T, idcol = T)
 
-dat <- dat %>%
+dat_all <- dat_all %>%
         dplyr::rename(., doc_id = .id, text = `.x[[i]]`) %>%
-        mutate(., doc_id = as.factor(sub(".*\\_ *(.*?) *.txt*", "\\1", doc_id))) 
+        mutate(., doc_id = as.factor(sub(".*\\_ *(.*?) *.txt*", "\\1", doc_id)),
+               language = ifelse(str_extract(doc_id, "[^.]+") == "US", "EN", str_extract(doc_id, "[^.]+")))
 
-dat <- dat %>%
+corpus_all <- dat_all %>%
         group_by(., doc_id) %>%
         summarise(., text = Reduce(paste, text)) %>%
-        mutate(., language = gsub("(?:.*/){2}([^_]+)_.*", "\\1", doc_id)) %>%
         as.data.frame(.) %>%
         DataframeSource(.) %>%
-        VCorpus(.)
-
-dat <- dat %>%
+        VCorpus(.) %>%
         tm_map(., content_transformer(tolower)) %>%
         tm_map(., content_transformer(removePunctuation)) %>%
         tm_map(., content_transformer(removeNumbers)) %>%
         tm_map(., content_transformer(stripWhitespace)) %>%
-        tm_map(., content_transformer(PlainTextDocument)) %>%
+        tm_map(., content_transformer(PlainTextDocument))
+
+lang <- unique(gsub("(?:.*/){2}([^_]+)_.*", "\\1", files))
+
+for (i in 1:length(lang)) {
+        assign(paste("corpus", lang[i], sep = "_"),
+               dat_all %>%
+                       subset(., .$language == toupper(lang[i])) %>%
+                       group_by(., doc_id) %>%
+                       summarise(., text = Reduce(paste, text)) %>%
+                       as.data.frame(.) %>%
+                       DataframeSource(.) %>%
+                       VCorpus(., readerControl = list(language = lang[i])) %>%
+                       tm_map(., content_transformer(tolower)) %>%
+                       tm_map(., content_transformer(removePunctuation)) %>%
+                       tm_map(., content_transformer(removeNumbers)) %>%
+                       tm_map(., content_transformer(stripWhitespace)) %>%
+                       tm_map(., content_transformer(PlainTextDocument))
+        )
+}
+
+n_gram_all <- corpus_all %>%
         DocumentTermMatrix(.) %>%
         removeSparseTerms(., 0.99)
         
 # Exploratory Analysis 
-top_words <- dat %>%
+top_words_all <- n_gram_all %>%
         as.matrix(.) %>%
         as.data.frame(.) %>%
         rownames_to_column(., var = "Doc")
 
-top_words <- top_words %>%
-        gather(., Word, Frequency, 2:ncol(top_words)) %>%
+top_words_all <- top_words_all %>%
+        gather(., Word, Frequency, 2:ncol(top_words_all)) %>%
         mutate(., Language = ifelse(str_extract(Doc, "[^.]+") == "US", "EN", str_extract(Doc, "[^.]+"))) %>%
         mutate_if(., is.character, as.factor) %>%
         group_by(., Language, Word) %>%
@@ -58,9 +77,9 @@ top_words <- top_words %>%
         group_by(., Language) %>%
         top_n(., 10, Frequency) %>%
         data_frame(.)
-        #arrange(., desc(Frequency), group_by = Language)
 
-freq.plot <- ggplot(top_words, aes(x = reorder(Word, -Frequency), y = Frequency, fill = Language)) + geom_bar(stat="identity", position="dodge") +
-        facet_wrap(~ Language, ncol = 2, scales='free_x') + theme_bw() + labs(title = "Top 10 Words by Language", x = "Words")
+freq.plot <- ggplot(top_words_all, aes(x = reorder(Word, -Frequency), y = Frequency, fill = Language)) + 
+        geom_bar(stat="identity", position="dodge") + facet_wrap(~ Language, ncol = 2, scales='free_x') + theme_bw() + 
+        labs(title = "Top 10 Words by Language", x = "Words")
 freq.plot 
 
