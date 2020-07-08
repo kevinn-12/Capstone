0403d4b8626735f6b76947bded94b69547c9992c
kevinn-12 <vdigueslacerda@gmail.com>
2020-07-08 13:01:38 -0300

    Removed stopwords by language

--- Capstone.R
+++ Capstone.R
@@ -1,5 +1,5 @@
 library(stringr); library(tm); library(dplyr); library(tibble); library(reshape); library(caret); library(RWeka); library(LaF)
-library(jsonlite); library(purrr); library(data.table); library(tidyr); library(ggplot2)
+library(jsonlite); library(purrr); library(data.table); library(tidyr); library(ggplot2); library(textcat)
 
 # Importing and CLeaning Data
 if (! file.exists("Coursera-SwiftKey.zip")) {
@@ -26,19 +26,10 @@ dat_all <- dat_all %>%
         mutate(., doc_id = as.factor(sub(".*\\_ *(.*?) *.txt*", "\\1", doc_id)),
                language = ifelse(str_extract(doc_id, "[^.]+") == "US", "EN", str_extract(doc_id, "[^.]+")))
 
-corpus_all <- dat_all %>%
-        group_by(., doc_id) %>%
-        summarise(., text = Reduce(paste, text)) %>%
-        as.data.frame(.) %>%
-        DataframeSource(.) %>%
-        VCorpus(.) %>%
-        tm_map(., content_transformer(tolower)) %>%
-        tm_map(., content_transformer(removePunctuation)) %>%
-        tm_map(., content_transformer(removeNumbers)) %>%
-        tm_map(., content_transformer(stripWhitespace)) %>%
-        tm_map(., content_transformer(PlainTextDocument))
-
 lang <- unique(gsub("(?:.*/){2}([^_]+)_.*", "\\1", files))
+token <- function(x) {
+        NGramTokenizer(x, Weka_control(min = 2, max = 4))
+}
 
 for (i in 1:length(lang)) {
         assign(paste("corpus", lang[i], sep = "_"),
@@ -53,13 +44,28 @@ for (i in 1:length(lang)) {
                        tm_map(., content_transformer(removePunctuation)) %>%
                        tm_map(., content_transformer(removeNumbers)) %>%
                        tm_map(., content_transformer(stripWhitespace)) %>%
-                       tm_map(., content_transformer(PlainTextDocument))
+                       tm_map(., content_transformer(PlainTextDocument)) %>%
+                       tm_map(., removeWords, stopwords(kind = lang[i])) %>%
+                       DocumentTermMatrix(., control = list(tokenize = token)) %>%
+                       removeSparseTerms(., 0.99) %>%
+                       as.matrix(.) %>%
+                       as.data.frame(.) %>%
+                       rownames_to_column(., var = "Doc") %>%
+                       gather(., Word, Frequency, names(.)[-1]) %>%
+                       mutate(., Language = ifelse(str_extract(Doc, "[^.]+") == "US", "EN", str_extract(Doc, "[^.]+"))) %>%
+                       mutate_if(., is.character, as.factor) %>%
+                       group_by(., Language, Word) %>%
+                       summarise(., Frequency = sum(Frequency)) %>%
+                       data.frame(.)
         )
 }
 
-n_gram_all <- corpus_all %>%
-        DocumentTermMatrix(.) %>%
-        removeSparseTerms(., 0.99)
+dat_all <- reshape::merge_all(list(corpus_de, corpus_en, corpus_fi, corpus_ru))
+rm(corpus_de, corpus_en, corpus_fi, corpus_ru)
+
+dat_all <- dat_all %>%
+        mutate(., N_gram = str_count(Word, "\\S+"))
+
         
 # Exploratory Analysis 
 top_words_all <- n_gram_all %>%
@@ -78,8 +84,8 @@ top_words_all <- top_words_all %>%
         top_n(., 10, Frequency) %>%
         data_frame(.)
 
-freq.plot <- ggplot(top_words_all, aes(x = reorder(Word, -Frequency), y = Frequency, fill = Language)) + 
+freq.plot_all <- ggplot(top_words_all, aes(x = reorder(Word, -Frequency), y = Frequency, fill = Language)) + 
         geom_bar(stat="identity", position="dodge") + facet_wrap(~ Language, ncol = 2, scales='free_x') + theme_bw() + 
         labs(title = "Top 10 Words by Language", x = "Words")
-freq.plot 
+freq.plot_all 
 
